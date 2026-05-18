7573b1ce2f849ae1f580f2affd31092ba95df689
LucasBunny <lucas08ms@gmail.com>
2026-05-18 20:01:41 -0300

    :heavy_plus_sign: feat: Acrescentando tabela barbeiros no Banco de Dados.

--- backend/database.sql
+++ backend/database.sql
@@ -23,3 +23,12 @@ CREATE TABLE agendamentos (
     FOREIGN KEY (cliente_id) REFERENCES clientes(id),
     FOREIGN KEY (servico_id) REFERENCES servicos(id)
 );
+
+CREATE TABLE barbeiros (
+  id int NOT NULL AUTO_INCREMENT,
+  nome varchar(100) NOT NULL,
+  senha varchar(100) NOT NULL,
+  criado_em timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  UNIQUE KEY nome (nome)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
